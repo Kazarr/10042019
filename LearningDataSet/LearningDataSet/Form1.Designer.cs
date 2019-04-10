eae5dc2b55318481ab156e36fa36261f8ef850e0
Matej Kardy Kardoš <matej.kardos@gmail.com>
2019-04-10 15:42:40 +0200

    Odpisane od mikiho... dobre veci ale naucit sa ich treba robit

---
+++ LearningDataSet/LearningDataSet/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace LearningDataSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdDataSet = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdDataSet
+            // 
+            this.grdDataSet.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdDataSet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDataSet.Location = new System.Drawing.Point(13, 13);
+            this.grdDataSet.Name = "grdDataSet";
+            this.grdDataSet.Size = new System.Drawing.Size(775, 425);
+            this.grdDataSet.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdDataSet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdDataSet;
+    }
+}
+
---
+++ LearningDataSet/LearningDataSet/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningDataSet
+{
+    public partial class Form1 : Form
+    {
+        const string CONNECTION_STRING = "Server = KARDOS\\SQLEXPRESSKARDY; Database=AdventureWorks;Trusted_Connection=True";
+        public Form1()
+        {
+            InitializeComponent();
+            grdDataSet.DataSource = FillDataset();
+            grdDataSet.DataMember = "Person";
+        }
+        public DataSet FillDataset()
+        {
+            SqlConnection connection = new SqlConnection(CONNECTION_STRING);
+            string query = @"SELECT [BusinessEntityID]
+                            ,[PersonType]
+                            ,[NameStyle]
+                            ,[Title]
+                            ,[FirstName]
+                            ,[MiddleName]
+                            ,[LastName]
+                            ,[Suffix]
+                            ,[EmailPromotion]
+                            ,[AdditionalContactInfo]
+                            ,[Demographics]
+                            ,[rowguid]
+                            ,[ModifiedDate]
+                        FROM [AdventureWorks].[Person].[Person]";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds, "Person");
+
+            DataTable dt = ds.Tables["Person"];
+            dt.Columns.Add("Name", typeof(string));
+            foreach(DataRow dr in dt.Rows)
+            {
+                dr.SetField("Name", $"{dr["LastName"]} {dr["MiddleName"]} {dr["FirstName"]}");
+            }
+
+            return ds;
+        }
+    }
+}
